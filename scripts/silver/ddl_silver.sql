63dfac4e66885de849049a65b7c2f739ce87c786
Phongdang12 <156529478+Phongdang12@users.noreply.github.com>
2025-06-04 21:38:35 +0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -16,9 +16,9 @@ IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
 
 CREATE TABLE silver.crm_sales_details (
-    sis_ord_num NVARCHAR(50),
-    sis_prd_key NVARCHAR(50),
-    sis_cust_id INT,
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
     sls_order_dt DATE,
     sls_ship_dt DATE,
     sls_due_dt DATE,
@@ -80,7 +80,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 
 );
-
 
 
 
